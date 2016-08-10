e1794b4e3c6370bddf2582139525aab2b6150b3c
Julian van Giessen <jsv@Julians-Air.fios-router.home>
2016-08-10 15:20:18 -0400

    short refactor for functional clarity on data-filter component

--- client/app/pods/components/data-filter/component.js
+++ client/app/pods/components/data-filter/component.js
@@ -7,39 +7,42 @@ import service from 'ember-service/inject';
 export default Component.extend({
   ajax: service(),
   noResults: false,
-  disabled: false,
   placeholder: reads('properties.placeholder'),
 
-  didReceiveAttrs() {
-    const ajax = get(this, 'ajax'),
-          props = get(this, 'properties'),
-          aggBy = props.filterAttr;
+  promise(that) {
+    const ajax = get(that, 'ajax'),
+          props = get(that, 'properties'),
+          aggBy = props.filterAttr,
+          params = Object.assign({aggBy}, props.currentParams);
 
-    const params = Object.assign({aggBy, 'limit': 10}, props.currentParams);
+    if ( ! params.q ) { delete params.q; }
 
-    function getData(that) {
-      if ( !params.q ) { delete params.q; }
-      return ajax.request(props.resource, {
-          method: 'GET',
-          data: params
-        }).then(result => {
-          if ( !that.isDestroyed ) {
-            set(that, 'promiseResults', result.data);
-          }
-          // no results
-          if ( result.data[0].id === '' ) {
-            set(that, 'noResults', true);
-            set(that, 'placeholder', 'No options available');
-            set(that, 'disabled', true);
-          }
-          return result.data;
-        }).catch(err => {
-          console.log(err);
-          return '-';
-        });
-    }
-
-    set(this, 'promise', getData(this));
+    return ajax.request(props.resource, {
+      method: 'GET',
+      data: params
+    }).then(result => {
+      if ( !that.isDestroyed ) {
+        set(that, 'promiseResults', result.data);
+      }
+      // no results or one null result
+      if ( (result.data[0].id === '') && (result.data.length <= 1) ) {
+        set(that, 'noResults', true);
+        set(that, 'placeholder', 'No options available');
+      // needs to be reset otherwise
+      } else {
+        set(that, 'noResults', false);
+        set(that, 'placeholder', props.placeholder);
+      }
+      return result.data;
+    }).catch(err => {
+      console.log('data-filter component error:', err);
+      return {id: 'error'};
+    });
   },
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+    set(this, 'results', get(this, 'promise')(this));
+  }
 
 });
